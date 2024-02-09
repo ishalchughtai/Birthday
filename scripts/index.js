db54d9342ddc58ab721d296cda43e27924bf7937
moiahm886 <moizahmedbss786@gmail.com>
2024-02-09 17:01:10 +0500

    changed

--- scripts/index.js
+++ scripts/index.js
@@ -4,8 +4,8 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Jan 30, 2024',
-  name: 'Ishal Chughtai'
+  birthdate: 'Feb 13, 2024',
+  name: 'Taha Khurram'
 };
 
 function hideEverything() {
